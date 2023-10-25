bdbe9f18157ae106a2af5cc5bf3608226bc3224c
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-10-25 12:13:25 +0530

    some remove and create text of group all notes and bookmark

---
+++ NotelyApp/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  NotelyApp
+//
+//  Created by Bhumika Patel on 25/10/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
